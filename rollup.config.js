fe91894d88e78e3318a8a339d02099d6566b8bd7
Zach Posten <zposten@gmail.com>
2019-02-06 13:26:31 -0600

    chore: Switch back to rollup babel
    It was missing some babel helper functions otherwise and I don't want to
    mess with that.

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,27 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import pkg from './package.json'
 import postcss from 'rollup-plugin-postcss'
+import babel from 'rollup-plugin-babel'
 
 export default [
 	// browser-friendly UMD build
-	{
-		input: 'babel/main.js',
-		output: {
-			name: 'photoswipeReact',
-			file: pkg.browser,
-			format: 'umd'
-		},
-		plugins: [
-			resolve(), // so Rollup can find `react`
-      commonjs(), // so Rollup can convert `react` to an ES module
-      postcss(), // so .css files can be imported
-		]
-	},
+	// {
+	// 	input: 'src/lib/main.js',
+	// 	output: {
+	// 		name: 'photoswipeReact',
+	// 		file: pkg.browser,
+	// 		format: 'umd'
+	// 	},
+	// 	plugins: [
+  //     // resolve(), // so Rollup can find `react`
+  //     // commonjs(), // so Rollup can convert `react` to an ES module
+  //     // postcss(), // so .css files can be imported
+  //     babel({
+  //       include: 'src/lib/*',
+  //       // runtimeHelpers: true,
+  //     }),
+	// 	]
+	// },
 
 	// CommonJS (for Node) and ES module (for bundlers) build.
 	// (We could have three entries in the configuration array
@@ -26,13 +31,18 @@ export default [
 	// an array for the `output` option, where we can specify 
 	// `file` and `format` for each target)
 	{
-		input: 'babel/main.js',
+		input: 'src/lib/main.js',
 		external: ['react'],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
 			{ file: pkg.module, format: 'es' }
     ],
     plugins: [
+      babel({
+        // include: 'src/lib/*',
+        exclude: 'node_modules/**',
+        // runtimeHelpers: true,
+      }),
       postcss(),
     ]
 	}
