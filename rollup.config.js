5d7f113c1bac7db0eb87d52dadac002918970ff8
Zach Posten <zach@posten.io>
2021-12-30 17:41:51 -0600

    chore: Convert build to TS

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,14 @@
-import pkg from './package.json'
-import postcss from 'rollup-plugin-postcss'
-import babel from 'rollup-plugin-babel'
+import path from 'path'
+import typescript from '@rollup/plugin-typescript'
 
-export default [
-	// browser-friendly UMD build
-	// {
-	// 	input: 'src/lib/main.js',
-	// 	output: {
-	// 		name: 'photoswipeReact',
-	// 		file: pkg.browser,
-	// 		format: 'umd'
-	// 	},
-	// 	plugins: [
-  //     // resolve(), // so Rollup can find `react`
-  //     // commonjs(), // so Rollup can convert `react` to an ES module
-  //     // postcss(), // so .css files can be imported
-  //     babel({
-  //       include: 'src/lib/*',
-  //       // runtimeHelpers: true,
-  //     }),
-	// 	]
-	// },
-
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify 
-	// `file` and `format` for each target)
-	{
-		input: 'src/lib/main.js',
-		external: ['react'],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-    ],
-    plugins: [
-      postcss({extract: true}),
-      babel({
-        // include: 'src/lib/*',
-        exclude: 'node_modules/**',
-        // runtimeHelpers: true,
-      }),
-    ]
-	}
-]
+export default {
+  input: 'src/index.ts',
+  output: {
+    format: 'es',
+    dir: 'dist',
+    sourcemap: true,
+  },
+  // Do not include any 3rd party libraries in the bundled code
+  external: id => !id.startsWith('.') && !path.isAbsolute(id),
+  plugins: [typescript()],
+}
