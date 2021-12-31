8c1659da8f95dd231d79f199b6f745ae8a1215f3
Zach Posten <zach@posten.io>
2021-12-31 02:16:32 -0600

    chore: Include both ESM and CJS builds

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,20 @@ import path from 'path'
 import typescript from '@rollup/plugin-typescript'
 import copy from 'rollup-plugin-copy'
 
+import pkg from './package.json'
+
 export default {
   input: ['src/index.ts'],
-  output: {
-    format: 'es',
-    dir: 'dist',
-  },
+  output: [
+    {
+      format: 'es',
+      file: pkg.module,
+    },
+    {
+      format: 'cjs',
+      dir: 'dist',
+    },
+  ],
   // Do not include any 3rd party libraries in the bundled code
   external: id => !id.startsWith('.') && !path.isAbsolute(id),
   plugins: [
